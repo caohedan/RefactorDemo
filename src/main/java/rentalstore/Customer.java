64d28eed5f126123b98f31c645e216d4666c3e64
caohedan <m18390990364@163.com>
2018-08-24 14:25:29 +0800

    [amelia]The first refactoring, the process of calculating the single lease credits and prices, is extracted and pumped into the Render class.

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -28,30 +28,8 @@ public class Customer {
             double thisAmount =0;
             Rental each = (Rental) rentals.nextElement();
 
-            switch (each.getMovie().getPriceCode()){
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if(each.getDayRented() > 2){
-                        thisAmount+=(each.getDayRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount+=each.getDayRented()*3;
-                    break;
-                    case Movie.CHILDRENS:
-                        thisAmount+=1.5;
-                        if(each.getDayRented() > 3){
-                            thisAmount += (each.getDayRented() -3)*1.5;
-                        }
-                        break;
-            }
-
-            //add frequent renter points
-            frequentRenterPoints ++;
-            //add bonus for a two day new release rental
-            if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1){
-                frequentRenterPoints ++;
-            }
+            thisAmount = each.getThisAmount(thisAmount);
+            frequentRenterPoints = each.getFrequentRenterPoints(frequentRenterPoints);
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
@@ -63,4 +41,5 @@ public class Customer {
         result += "You earned" + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
+
 }
--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -1,11 +1,8 @@
 package rentalstore;
 
-import org.junit.Assert;
+
 import org.junit.Test;
 
-import java.io.Reader;
-
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class CustomerTest {
